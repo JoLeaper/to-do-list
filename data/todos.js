2fe48e32c47909f98c2f7cc71f1f0f904e4ebada
JoLeaper <joleap123@gmail.com>
2020-05-12 16:35:50 -0700

    database set-up

---
+++ data/todos.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    task: 'Take Out Garbage',
+    priority_level: 1,
+    completed: false
+  },
+  {
+    task: 'Jog',
+    priority_level: 2,
+    completed: false
+  },
+
+];
